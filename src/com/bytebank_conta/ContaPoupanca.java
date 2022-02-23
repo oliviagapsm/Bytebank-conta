72d3554a55f448c513a7eda75a0c5c893bda1564
oliviagapsm <olivia.gonzalezmacedo@gmail.com>
2022-02-23 08:31:49 -0300

    Primeiro commit contas corrente e poupança

---
+++ src/com/bytebank_conta/ContaPoupanca.java
@@ -0,0 +1,12 @@
+package com.bytebank_conta;
+
+public class ContaPoupanca extends Conta{
+    public ContaPoupanca(int agencia, int numero){
+        super(agencia, numero);
+    }
+
+    @Override
+    public void deposita(double valor) {
+        super.saldo += valor;
+    }
+}
---
+++ src/com/bytebank_conta/TesteConta.java
@@ -0,0 +1,17 @@
+package com.bytebank_conta;
+
+public class TesteConta {
+    public static void main(String[] args) {
+
+        ContaCorrente cc = new ContaCorrente(555, 203);
+        cc.deposita(100.0);
+
+        ContaPoupanca cp = new ContaPoupanca(678, 907);
+        cp.deposita(200.0);
+
+        cc.transfere(50.0, cp);
+
+        System.out.println("CC: " + cc.getSaldo());
+        System.out.println("CP: " + cp.getSaldo());
+    }
+}
